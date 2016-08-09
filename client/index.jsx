9e97bfd57d607d04f5eb321616a26b05a0e75937
Trevor Healy <trevorwhealy@gmail.com>
2016-08-08 19:30:35 -0700

    establish components folder and create navbar file

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Navbar from './components/navbar';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
