95bd41edb1bd645201f7f3ab95b10fa38961e461
Trevor Healy <trevorwhealy@gmail.com>
2016-08-10 12:04:39 -0700

    chore add styling to the sidebar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -8,26 +8,38 @@ const folders = ['chemistry', 'biology', 'java', 'c#', 'python'];
 
 const Navbar = () => (
   <div>
+
     <ul id="slide-out" className="side-nav">
+      {/* User Includes userPhoto, userName, and userLogout */}
       <div className="user">
-        <img className="circle" src='/assets/images/sunnyv.jpg' width="30" height="30"/>
+        <img className="circle" src='/assets/images/sunnyv.jpg' width="35" height="35"/>
         <div className="name">{'Sunny V'}</div>
-        <i className="material-icons alert">info</i>
+        <i className="material-icons alert">power_settings_new</i>
       </div>
 
-      <div className="folder">
+      {/*  createFolder includes title and create button */}
+      <div className="createFolder">
         <div className="title"> FOLDERS </div>
-        <div className="add">NEW<i className="material-icons alert">add</i></div>
+        <div className="add">NEW<i className="tiny material-icons alert">add</i></div>
+      </div>
+
+      {/* folderNames includes the names of the folders */}
+      <div className="folderNames">
         {folders.map(folder => {
           // TODO: decide how many folders we want to display - most recent 5? all of them?
           return (
-            <div>
+            <div className="folder">
               {folder}
             </div>
           );
         })}
       </div>
-      {'search'}
+
+      {/* Search is the search bar at the bottom of the page */}
+      <div className="search">
+        <div className="title"> SEARCH </div>
+        <input className="query" type="text" placeholder="Find notes by Name or Tags"/>
+      </div>
 
     </ul>
     <a href="#" data-activates="slide-out" className="button-collapse"><i className="material-icons">menu</i></a>
