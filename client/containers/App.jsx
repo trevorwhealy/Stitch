6b6d3ca4fb0dc315e702a24ddb7e484e0ba798e9
Trevor Healy <trevorwhealy@gmail.com>
2016-08-10 17:15:12 -0700

    add Sidebar and jQuery logic to app.jsx componentDidMount

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+
+import Sidebar from '../components/Sidebar.jsx';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  componentDidMount() {
+    $('.button-collapse').sideNav({
+      menuWidth: 300,
+      edge: 'left',
+      closeOnClick: true
+    });
+  }
+
   render() {
     return (
       <div>
+        <Sidebar />
         <h1>Homepages</h1>
         <ul>
           <li><Link to="/login">Login</Link></li>
