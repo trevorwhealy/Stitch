09312f32775da61ab441042e5ce56c6eb2d278b4
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-08-11 19:09:20 -0700

    move models sync into sequelize config

--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(config.db, { logging: true });
 
 module.exports = sequelize;
 
-/* Models */
+/* Load all models */
 require('../users/user.model');
 require('../folders/folder.model');
 require('../shares/share.model');
@@ -15,12 +15,5 @@ require('../comments/comment.model');
 
 sequelize.authenticate()
   .then(() => console.log('Database is connected'))
-  .then(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      /* eslint-disable global-require */
-      require('../seeds');
-      /* eslint-enable global-require */
-    }
-  })
   .then(() => sequelize.sync())
   .catch(err => console.error('DB Error: ', err));
