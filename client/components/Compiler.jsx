57a1ad872d39c3349bdaa58ae4331c200cd9bcb2
Trevor Healy <trevorwhealy@gmail.com>
2016-08-12 12:29:30 -0700

    fix: improve styling

--- client/components/Compiler.jsx
+++ client/components/Compiler.jsx
@@ -13,20 +13,31 @@ class Compiler extends React.Component {
       loader: false,
       run: false,
       answer: '',
+      languages: ['python', 'ruby', 'javascript'],
     };
   }
 
   compile(e) {
     e.preventDefault();
+    const code = document.getElementById('codeToRun').value;
+    let lang = document.getElementById('language').value;
+
+    // If no code typed, or no language selected, do not compile
+    if (code.length < 1 || lang.length < 1) {
+      return;
+    }
+
+    // Convert JS to node
+    if (lang === 'javascript') {
+      lang = 'node';
+    }
+
     this.setState({
       loader: true,
       run: true,
     });
 
     const jwtToken = localStorage.getItem('jwtToken');
-
-    const code = document.getElementById('codeToRun').value;
-    const lang = document.getElementById('language').value;
 
     fetch('http://localhost:3030/run', {
       method: 'POST',
@@ -42,14 +53,16 @@ class Compiler extends React.Component {
     .then(res => res.json())
     .then(data => {
       console.log(data.stdout.indexOf('/usr/src/app/'));
+
+      // if /usr/src/app exists in the output
       if (data.stdout.indexOf('/usr/src/app/') !== -1) {
-        //const SyntaxError = data.stdout.substring(data.stdout.indexOf(','));
-        console.log(data.stdout);
+        console.log('entire', data.stdout);
+
         const ErrorLocation = data.stdout.substring(data.stdout.indexOf(',') + 2, data.stdout.indexOf('^') + 1);
         const SyntaxError = data.stdout.substring(data.stdout.indexOf('SyntaxError: '));
-
         const ErrorMessage = `${SyntaxError} \n ${ErrorLocation}`;
-        console.log(ErrorMessage);
+
+        console.log('error msg', ErrorMessage);
       } else {
         this.setState({
           loader: false,
@@ -66,15 +79,17 @@ class Compiler extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="compilerContainer">
         {console.log(this.state.loader)}
         <select id="language">
           <option value="" disabled selected>Choose your language</option>
-          <option value="node">Javascript</option>
-          <option value="python">Python</option>
-          <option value="ruby">Ruby</option>
+          {this.state.languages.map(language => {
+            return (
+              <option value={`${language}`}>{language}</option>
+            );
+          })}
         </select>
-        <textarea rows={10} id="codeToRun" type="text" name="code" />
+        <textarea className="materialize-textarea" id="codeToRun" type="text" name="code" />
         {
           this.state.loader ? <div className="progress"><div className="indeterminate" /></div> : ''
         }
