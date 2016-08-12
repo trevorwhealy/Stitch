fa50fdde1be1304c1fe969c12345a9790577eb2d
Trevor Healy <trevorwhealy@gmail.com>
2016-08-11 21:51:46 -0700

    chore: add select for different languages

--- client/components/Compiler.jsx
+++ client/components/Compiler.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+$(document).ready(function() {
+   $('select').material_select();
+});
+
 const Compiler = () => {
 
   let globalVariable;
@@ -10,10 +14,14 @@ const Compiler = () => {
     var jwtToken = localStorage.getItem('jwtToken');
 
     const code = document.getElementById('codeToRun').value;
+    const lang = document.getElementById('language').value;
+
+    console.log(lang);
+
     fetch('http://localhost:3030/run', {
       method: 'POST',
       body: JSON.stringify({
-        language: 'node',
+        language: lang,
         content: code,
       }),
       headers: {
@@ -22,11 +30,29 @@ const Compiler = () => {
       },
     })
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => {
+      if (data.stdout.indexOf('/usr/src/app/')) {
+        const SyntaxError = data.stdout.substring(data.stdout.indexOf(','));
+        console.log(data.stdout);
+        console.log(SyntaxError);
+      } else {
+        console.log(data.stdout);
+      }
+      //var errorIndicator = data.stdout.substring(0,19);
+      //if (data.stdout.substring(0, 12));
+      //console.log(data.stdout)
+    });
   };
 
   return (
     <div>
+      <select id="language">
+        <option value="" disabled selected>Choose your language</option>
+        <option value="node">Javascript</option>
+        <option value="python">Python</option>
+        <option value="ruby">Ruby</option>
+      </select>
+
       <textarea id="codeToRun" type="text" name="code" />
       <button onClick={compile}>Compile</button>
     </div>
