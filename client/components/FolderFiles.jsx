ecbc2196ac100a2cb100ac78b838a7f58c3ff4c0
Trevor Healy <trevorwhealy@gmail.com>
2016-08-13 11:47:42 -0700

    add pasic page format

--- client/components/FolderFiles.jsx
+++ client/components/FolderFiles.jsx
@@ -5,11 +5,23 @@ class FolderFiles extends React.Component {
     super();
   }
 
+  const fakeNotes = [
+
+
+  ];
+
   render() {
     return (
       <div className="folderFiles">
-        
-        heyyyy
+        <div className="title">Folder Name</div>
+        <div className="number">Number of notes</div>
+        <div className="notes"> {fakeNotes.map(note => {
+            // any additional logic
+            return (
+
+            );
+          })}
+        </div>
       </div>
     );
   }
