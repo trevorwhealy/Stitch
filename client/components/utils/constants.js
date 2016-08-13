a4a6a33ce95e6891a568df3541262f0b327d26b2
hewp <steventlin.9@gmail.com>
2016-08-13 14:40:06 -0700

    added rich editor basics

---
+++ client/components/utils/constants.js
@@ -0,0 +1,21 @@
+export const BLOCK_TYPES = [
+  {label: 'H1', style: 'header-one'},
+  {label: 'H2', style: 'header-two'},
+  {label: 'H3', style: 'header-three'},
+  {label: 'Blockquote', style: 'blockquote'},
+  {label: 'UL', style: 'unordered-list-item'},
+  {label: 'OL', style: 'ordered-list-item'},
+  {label: 'Code Block', style: 'code-block'},
+];
+
+
+export const INLINE_STYLES = [
+  {label: 'BOLD', style: 'BOLD'},
+  {label: 'ITALIC', style: 'ITALIC'},
+  {label: 'UNDERLINE', style: 'UNDERLINE'},
+];
+
+export default {
+  BLOCK_TYPES,
+  INLINE_STYLES,
+};
