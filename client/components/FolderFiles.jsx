efaa330c883a8e043703a5e25f1651f37d7ae407
Trevor Healy <trevorwhealy@gmail.com>
2016-08-13 11:33:19 -0700

    add route, style, and jsx files for folderfiles

---
+++ client/components/FolderFiles.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class FolderFiles extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="folderFiles">
+        
+        heyyyy
+      </div>
+    );
+  }
+}
+
+
+export default FolderFiles;
