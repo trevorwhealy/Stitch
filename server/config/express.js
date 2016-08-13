ec4bdaee17c7b8d0e5b0123608fdcd4100fd6364
isunnyvirk <isunnyvirk@gmail.com>
2016-08-13 12:04:47 -0700

    chore fixing master

--- server/config/express.js
+++ server/config/express.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+<<<<<<< HEAD
+const webpack = require('webpack');
+const webpackConfig = require('../../webpack.config');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+=======
 const passport = require('passport');
+>>>>>>> c4e71ec1dab24ef4f6f132387928d89e3fa8da9f
 
 module.exports = (app, config) => {
   // logging for request on the console
@@ -12,6 +19,18 @@ module.exports = (app, config) => {
   app.use(passport.initialize());
 
   // serving static files to client
+<<<<<<< HEAD
+  const path = '/public';
+  app.use(express.static(config.rootPath + path));
+
+  // Use webpack HMR in dev
+  if (process.env.NODE_ENV !== 'production') {
+    // webpack
+    const compiler = webpack(webpackConfig);
+
+    app.use(webpackDevMiddleware(compiler,
+     { noInfo: true, publicPath: webpackConfig.output.publicPath }));
+=======
   app.use(express.static(`${config.rootPath}/public`));
 
   // Use webpack HMR in dev
@@ -28,6 +47,7 @@ module.exports = (app, config) => {
       noInfo: true,
       publicPath: webpackConfig.output.publicPath,
     }));
+>>>>>>> c4e71ec1dab24ef4f6f132387928d89e3fa8da9f
     app.use(webpackHotMiddleware(compiler));
   }
 };
