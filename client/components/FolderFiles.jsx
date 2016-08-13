4617f452b9a8fdbeb3682a09e3fb7829aab02586
Trevor Healy <trevorwhealy@gmail.com>
2016-08-13 13:57:41 -0700

    add momentjs and minor styling

--- client/components/FolderFiles.jsx
+++ client/components/FolderFiles.jsx
@@ -1,27 +1,65 @@
 import React from 'react';
+import moment from 'moment';
+
+const fakeNotes = [
+  {
+    title: 'flexbox froggy',
+    date: new Date(),
+  },
+  {
+    title: 'flexbox froggy',
+    date: new Date(),
+  },
+  {
+    title: 'flexbox froggy',
+    date: new Date(),
+  },
+  {
+    title: 'flexbox froggy',
+    date: new Date(),
+  },
+  {
+    title: 'flexbox froggy',
+    date: new Date(),
+  },
+  {
+    title: 'flexbox froggy',
+    date: new Date(),
+  },
+  {
+    title: 'flexbox froggy',
+    date: new Date(),
+  },
+  {
+    title: 'flexbox froggy',
+    date: new Date(),
+  },
+];
 
 class FolderFiles extends React.Component {
   constructor() {
     super();
   }
 
-  const fakeNotes = [
-
-
-  ];
-
   render() {
     return (
       <div className="folderFiles">
-        <div className="title">Folder Name</div>
-        <div className="number">Number of notes</div>
+        <div className="title">{'Folder Name'}</div>
+        <div className="number">{`${fakeNotes.length} notes found`}</div>
         <div className="notes"> {fakeNotes.map(note => {
-            // any additional logic
-            return (
-
-            );
+          return (
+            <div className="note">
+              <div className="details">
+                <div className="name">{note.title}</div>
+                <div className="date">{moment().startOf(note.date).fromNow()}</div>
+              </div>
+              <div className="open">{'OPEN'}</div>
+            </div>
+          );
           })}
         </div>
+        <div className="prompt">{'Scroll for more'}</div>
+        <div><i className="material-icons">keyboard_arrow_down</i></div>
       </div>
     );
   }
