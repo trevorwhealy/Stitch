fa5729e650d8c2c4a97c357ae3706da65521dcd3
isunnyvirk <isunnyvirk@gmail.com>
2016-08-13 12:06:45 -0700

    fix rebase issue

--- server/config/express.js
+++ server/config/express.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-<<<<<<< HEAD
-const webpack = require('webpack');
-const webpackConfig = require('../../webpack.config');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-=======
 const passport = require('passport');
->>>>>>> c4e71ec1dab24ef4f6f132387928d89e3fa8da9f
 
 module.exports = (app, config) => {
   // logging for request on the console
@@ -19,18 +12,6 @@ module.exports = (app, config) => {
   app.use(passport.initialize());
 
   // serving static files to client
-<<<<<<< HEAD
-  const path = '/public';
-  app.use(express.static(config.rootPath + path));
-
-  // Use webpack HMR in dev
-  if (process.env.NODE_ENV !== 'production') {
-    // webpack
-    const compiler = webpack(webpackConfig);
-
-    app.use(webpackDevMiddleware(compiler,
-     { noInfo: true, publicPath: webpackConfig.output.publicPath }));
-=======
   app.use(express.static(`${config.rootPath}/public`));
 
   // Use webpack HMR in dev
@@ -47,7 +28,6 @@ module.exports = (app, config) => {
       noInfo: true,
       publicPath: webpackConfig.output.publicPath,
     }));
->>>>>>> c4e71ec1dab24ef4f6f132387928d89e3fa8da9f
     app.use(webpackHotMiddleware(compiler));
   }
 };
