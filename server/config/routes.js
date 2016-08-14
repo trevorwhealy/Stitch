866b5fc25bf2c7ec209e4d80cb7a47a57afa2f3d
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-08-13 17:24:40 -0700

    add getOne note route and refactor note api url to just /notes

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 
 const AuthRouter = require('../auth/auth.routes');
 const FolderRouter = require('../folders/folder.routes');
+const NoteRouter = require('../notes/note.routes');
 const CommentRouter = require('../comments/comment.routes');
 
 module.exports = (app, config) => {
@@ -9,6 +10,7 @@ module.exports = (app, config) => {
 
   app.use('/api', passport.authenticate('jwt', { session: false }));
   app.use('/api/folders', FolderRouter);
+  app.use('/api/notes', NoteRouter);
   app.use('/api/comments', CommentRouter);
 
   app.get('/*', (req, res) => {
--- server/notes/note.routes.js
+++ server/notes/note.routes.js
@@ -1,13 +1,18 @@
 const NoteRouter = require('express').Router();
 const NoteCtrl = require('./note.controller');
 
-NoteRouter.route('/:folderId/notes')
+const ShareCtrl = require('../shares/share.controller');
+
+NoteRouter.route('/')
   .get(NoteCtrl.getAll)
   .post(NoteCtrl.post);
 
-NoteRouter.route('/:folderId/notes/:id')
+NoteRouter.route('/:id')
   .get(NoteCtrl.getOne)
   .put(NoteCtrl.put)
   .delete(NoteCtrl.deleteOne);
+
+NoteRouter.post('/:noteId/share', ShareCtrl.shareNote);
+NoteRouter.post('/:noteId/unshare', ShareCtrl.unshareNote);
 
 module.exports = NoteRouter;
