c3e9a70e77362b14ac39366648cc997bf7b97176
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-08-16 11:21:21 -0700

    add sort and note count to GET folders endpoint

--- server/folders/folder.model.js
+++ server/folders/folder.model.js
@@ -1,15 +1,18 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/sequelize');
 
-const User = require('../users/user.model');
-const Share = require('../shares/share.model');
-
 const Folder = sequelize.define('folder', {
   name: { type: Sequelize.STRING, required: true },
   userId: { type: Sequelize.INTEGER, required: true },
 });
 
+module.exports = Folder;
+
+/* Setup Associations */
+const User = require('../users/user.model');
+const Share = require('../shares/share.model');
+const Note = require('../notes/note.model');
+
 Folder.belongsTo(User);
 Folder.hasMany(Share);
-
-module.exports = Folder;
+Folder.hasMany(Note);
