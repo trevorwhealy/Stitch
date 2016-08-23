6ae67bd27a7bc20238a928006d5f5850dd5c53df
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-08-23 10:29:59 -0700

    add failureRedirect to /login

--- server/auth/auth.routes.js
+++ server/auth/auth.routes.js
@@ -7,6 +7,9 @@ AuthRouter.post('/login', AuthCtrl.login);
 AuthRouter.post('/signup', AuthCtrl.signup);
 AuthRouter.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 AuthRouter.get('/google/callback',
-  passport.authenticate('google', { session: false }), AuthCtrl.oauthSuccess);
+  passport.authenticate('google', {
+    session: false,
+    failureRedirect: '/login',
+  }), AuthCtrl.oauthSuccess);
 
 module.exports = AuthRouter;
