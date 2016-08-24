87cf2187e694bb2acb27f3693993ed900d65fcd0
isunnyvirk <isunnyvirk@gmail.com>
2016-08-24 15:43:30 -0700

    chore file structure changes

--- __tests__/sum-test.js
+++
@@ -1,8 +0,0 @@
-jest.unmock('../client/sum'); // unmock to use the actual implementation of sum
-
-describe('sum', () => {
-  it('adds 1 + 2 to equal 3', () => {
-    const sum = require('../client/sum');
-    expect(sum(1, 2)).toBe(3);
-  });
-});
