2b77f37fd5b8770a241304c68e0226b9baa63166
Trevor Healy <trevorwhealy@gmail.com>
2016-08-26 10:38:11 -0700

    optimize webpack config for produciton

---
+++ webpack.prod.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  context: path.join(__dirname, './client'),
+  entry: [
+    './index.jsx',
+  ],
+  output: {
+    path: path.resolve(__dirname, './public/dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"',
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style', 'css',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'postcss', 'sass'],
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'url?prefix=font/&limit=5000',
+      },
+    ],
+  },
+  postcss: () => {
+    return [autoprefixer];
+  },
+};
