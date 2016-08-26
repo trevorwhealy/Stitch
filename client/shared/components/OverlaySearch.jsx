378f5c57922ec770f6c4404530d43672613d39f2
isunnyvirk <isunnyvirk@gmail.com>
2016-08-25 20:50:30 -0700

    ui improvements

--- client/shared/components/OverlaySearch.jsx
+++ client/shared/components/OverlaySearch.jsx
@@ -10,12 +10,17 @@ class OverlaySearch extends React.Component {
     this.state = {
       isOverLayClicked: false,
       isActive: false,
+      choice: 'folder',
     };
     this.displayOverlay = this.displayOverlay.bind(this);
     this.fileChoice = this.fileChoice.bind(this);
     this.folderChoice = this.folderChoice.bind(this);
     this.searchInput = this.searchInput.bind(this);
     this.displayOverlay = this.displayOverlay.bind(this);
+  }
+
+  componentDidMount() {
+    $('.dropdown-buttonTwo').dropdown({});
   }
 
   displayOverlay() {
@@ -51,6 +56,11 @@ class OverlaySearch extends React.Component {
     });
   }
 
+  test() {
+    console.log('hello');
+    $('.dropdown-buttonTwo').dropdown('open');
+  }
+
   render() {
     let display;
     if (this.state.isOverLayClicked) {
@@ -64,17 +74,23 @@ class OverlaySearch extends React.Component {
           <div className="overlay-responsive">
             <div className="searchFileOrFolder">
               <i className="material-icons searchIcon">search</i>
-              <div className="fileOrFolderChoice">
-                <div className="userSearchFile isActive" onClick={this.fileChoice}>
-                  {'File'}
-                </div>
-                <div className="userSearchFolder" onClick={this.folderChoice}>
-                  {'Folder'}
-                </div>
+              <div className="searchAllInput">
+                <input className="userQuery" type="text" onKeyDown={this.searchInput} />
               </div>
-            </div>
-            <div className="searchAllInput">
-              <input className="userQuery" type="text" onKeyDown={this.searchInput} />
+              <div className="fileOrFolderChoice">
+                <div className="fileOrFolderLabel">
+                  {this.state.choice}
+                </div>
+                <a className="dropdown-buttonTwo icon-btn" data-activates="userChoiceDropDown" onClick={this.test}>
+                  <i className="material-icons">keyboard_arrow_down</i>
+                </a>
+                <ul id="userChoiceDropDown" className="dropdown-content">
+                  <li><a href="#!">one</a></li>
+                  <li><a href="#!">two</a></li>
+                  <li className="divider"></li>
+                  <li><a href="#!">three</a></li>
+                </ul>
+              </div>
             </div>
           </div>
           <OverlaySearchResults toggleOverlay={this.displayOverlay} />
