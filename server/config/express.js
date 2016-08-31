95660f78db97d0fd830fc3c0705f1a4ff8684e04
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-08-31 09:53:32 -0700

    add sslify middleware to enforce https in prod

--- server/config/express.js
+++ server/config/express.js
@@ -3,17 +3,19 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const passport = require('passport');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 module.exports = (app, config) => {
-  // logging for request on the console
   app.use(morgan(config.apiLogLevel));
-
-  // middleware for parsing
   app.use(bodyParser.json());
   app.use(passport.initialize());
-  app.use(compression())
+  app.use(compression());
 
-  // serving static files to client
+  // Enforce HTTPS in production
+  if (process.env.NODE_ENV === 'production') {
+    app.use(enforce.HTTPS());
+  }
+
   app.use(express.static(`${config.rootPath}/public`));
 
   // Use webpack HMR in dev
